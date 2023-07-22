5a19cf9f133ef84ed237001d765fff6c5700df46
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2023-07-22 11:15:04 +0600

    scss & react

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,22 @@
 import webpack from "webpack";
 
 export const buildLoaders = (): webpack.RuleSetRule[] => {
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
   const tsLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return [tsLoader];
+  return [tsLoader, scssLoader];
 };
--- src/test.ts
+++
@@ -1,3 +0,0 @@
-export const test = (v: string) => {
-  console.log("TEST", v);
-};
