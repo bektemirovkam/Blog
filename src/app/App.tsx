2924d76b09c7457ff1f50731a2d16b994a64e958
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2023-07-24 19:57:48 +0600

    routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +13,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
-      <Suspense fallback={<div>loading...</div>}>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
