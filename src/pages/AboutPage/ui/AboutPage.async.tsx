333c01fd58834a1b9e099a025bb165f9f53e356c
bektemirovkam <55695037+bektemirovkam@users.noreply.github.com>
2023-08-09 20:38:05 +0600

    my setup eslint & stylelint

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,7 +1,9 @@
-import { lazy } from 'react';
+import {lazy} from 'react';
 
-export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./AboutPage')), 1500);
+export const AboutPageAsync = lazy(async () => new Promise(resolve => {
+	setTimeout(() => {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error
+		resolve(import('./AboutPage'));
+	}, 1500);
 }));
--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,7 +1,12 @@
-import { lazy } from 'react';
+import {lazy} from 'react';
 
-export const MainPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./MainPage')), 1500);
-}));
+export const MainPageAsync = lazy(
+	async () =>
+		new Promise(resolve => {
+			setTimeout(() => {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-expect-error
+				resolve(import('./MainPage'));
+			}, 1500);
+		}),
+);
